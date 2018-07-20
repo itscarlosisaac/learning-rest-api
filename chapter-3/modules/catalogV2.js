cca628e43376cffa3fe60e410c4d78e7ad61e567
Carlos Martinez <itscarlosisaac@gmail.com>
2018-07-20 17:04:15 -0400

    Added attributes filter to v2

--- chapter-3/modules/catalogV2.js
+++ chapter-3/modules/catalogV2.js
@@ -158,48 +158,27 @@ exports.findAllItems = function(response){
   });
 }
 
-// LOCALLY
-
-
-// function readCatalogSync(){
-//   var file = path.join(__dirname, 'data/catalog.json');
-//   if(fs.existsSync(file)){
-//     var content = fs.readFileSync(file);
-//     var catalog = JSON.parse(content);
-//     return catalog;
-//   }
-//   return undefined;
-// }
-
-// exports.findItems = function(categoryId){
-//   console.log('Return all items for the category Id: ', categoryId );
-//   var catalogs = readCatalogSync();
-//   if( catalogs ){
-//     let items = []
-//     catalogs.catalog.map( obj => obj.categoryId === categoryId ? items.push(...obj["items"] ) : [] );
-//     return items;
-//   }
-//   return undefined;
-// }
-
-// exports.findItem = function(categoryId, itemId){
-//   console.log('Return an item with the id:', itemId)
-//   var catalog = readCatalogSync();
-//   if( catalog ){
-//     let cat = catalog.catalog.filter( item => item.categoryId == categoryId );
-//     let product = cat[0].items.filter( item => item.itemId === itemId )
-//     return product[0]
-//   }
-//   return undefined
-// }
-
-// exports.findCategories = function(){
-//   console.log('Returning all categories');
-//   var catalog = readCatalogSync();
-//   if( catalog ){
-//     return catalog.catalog.map((item) => {
-//       return { categoryId: item["categoryId"], categoryName: item["categoryName"] }
-//     })
-//   }
-//   return []
-// }
+exports.findItemByAttribute = function(filter, response ) {
+  // let filter = {};
+  // filter[key] = value;
+  CatalogItem.find(filter, function(error, result) {
+    if( error){
+      console.log(error);
+      response.writeHead(500, contentTypePlainText);
+      response.end('Internal Server error');
+      return;
+    }else{
+      if( !result ){
+        if( response != null ){
+          response.writeHead(200, contentTypeJson);
+          response.end();
+        }
+        return
+      }
+      if( response != null ){
+        response.setHeader('Content-Type', 'application/json');
+        response.send(result);
+      }
+    }
+  })
+}
--- chapter-3/routes/catalog.js
+++ chapter-3/routes/catalog.js
@@ -1,14 +1,30 @@
 const router = require('express').Router()
 const catalog = require('../modules/catalogV1')
+const catalogV2 = require('../modules/catalogV2')
 const model = require('../model/item');
+const url = require('url')
 
 router.get('/v1/', function(request, response, next){
   catalog.findAllItems(response);
 });
 
-router.get('/v1/item/:itemId', function(request, response, next){
+router.get('/v1/items/:itemId', function(request, response, next){
   console.log(`${request.url} querying for ${request.params.itemId}`)
   catalog.findItemById(request.params.itemId, response)
+})
+
+router.get('/v2/items/', function(request, response, next){
+  var getParams = url.parse(request.url, true).query;
+  if( Object.keys(getParams).length === 0){
+    catalogV2.findAllItems(response);
+
+  }else{
+    console.log(getParams)
+    var key = Object.keys(getParams);
+    var value = getParams[key];
+
+    catalogV2.findItemByAttribute( getParams, response)
+  }
 })
 
 router.get('/v1/:categoryId', function(request, response, next){
