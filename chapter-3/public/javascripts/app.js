e82721ab5582f7159ee3b86d458dd991af6fd435
Carlos Martinez <itscarlosisaac@gmail.com>
2018-08-08 17:15:29 -0400

    Added form to add data.

--- chapter-3/public/javascripts/app.js
+++ chapter-3/public/javascripts/app.js
@@ -5,63 +5,36 @@ $(document).ready(function(){
   })
 
 
-  // // Posting Data
-  // var newItem = {
-  //   "currency": "USD",
-  //   "price": 564,
-  //   "itemId": 5442,
-  //   "itemName": "Sport FrontEnd",
-  //   "categories":[
-  //     "Main", "Watches"
-  //   ]
-  // }
-  // $.ajax({
-  //   url: "http://localhost:3000/catalog/v2",
-  //   type: 'POST',
-  //   dataType: "json",
-  //   data: newItem,
-  //   success: function(data, status, xhr){
-  //     if( status === 'success'){
-  //       console.log( data )
-  //       console.log( xhr )
-  //     }
-  //   },
-  //   error: function(xhr, options, error){
-  //     console.log("ERROR", error)
-  //   }
-  // })
-
-    // $.ajax({
-    //   contentType:'application/json',
-    //   url: 'http://localhost:3000/catalog/v2',
-    //   type: 'GET',
-    //   success: function( data, status, xhr ){
-    //     if( status === 'success'){
-    //       console.log(data)
-    //       data.docs.map(  (item) => {
-    //         var { itemName, itemId, price, currency, categories } = item
-    //         $('#dataTable tbody').append(
-    //           $(`
-    //             <tr id="${itemId}">
-    //               <td>${itemName}</td>
-    //               <td>${categories.join(' | ')}</td>
-    //               <td>${price}</td>
-    //               <td> ${currency}</td>
-    //               <td><input type="button" class="delete" value="Delete" data-id=${itemId} /></td>
-    //             </tr>
-    //           `)
-    //         )
-    //       })
-    //     }
-    //   },
-    //   error: function(xhr, options, error){
-    //     console.log("ERROR")
-    //   },
-    //   done: function(){
-    //     addDeleteAction();
-    //     console.log('ACTION')
-    //   }
-    // })
+  
+  $("#add").on('click', function(){
+    var id = Math.ceil(Math.random(0,1) * 10000)
+    console.log(id)
+    // // Posting Data
+    var newItem = {
+      "currency": $(".currency").val(),
+      "price": $(".price").val(),
+      "itemId": id,
+      "itemName": $(".name").val(),
+      "categories":[]
+    }
+    $.ajax({
+      url: "http://localhost:3000/catalog/v2",
+      type: 'POST',
+      dataType: "json",
+      data: newItem,
+      success: function(data, status, xhr){
+        if( status === 'success'){
+          console.log( data )
+          console.log( xhr )
+        }
+        $('input[type="text"]').val("")
+      },
+      error: function(xhr, options, error){
+        console.log("ERROR", error)
+      }
+    })
+  })
+  
     $.ajax({
       contentType:'application/json',
       url: 'http://localhost:3000/catalog/v2',
@@ -86,12 +59,12 @@ $(document).ready(function(){
         })
         }
       })
-      // error: function(xhr, options, error){
-      //   console.log("ERROR")
-      // },
       .then( function(){
         addDeleteAction();
         console.log('ACTION')
+      })
+      .fail(function(xhr, options, error){
+        console.log("ERROR")
       })
     
 
